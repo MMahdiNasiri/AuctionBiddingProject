46b9367b705cfc43c41ad0b5aab143d43bec5168
m.nasiri <mohammadmahdi.nasiri@digikala.ir>
2023-05-18 21:35:25 +0330

    feat: Create Products App
    - Create products app
    - Design products app data model

--- AuctionBidding/settings.py
+++ AuctionBidding/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'products.apps.ProductsConfig',
 
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ products/models.py
@@ -0,0 +1,60 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    logo = models.ImageField(upload_to='logo')
+
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=30)
+    logo = models.ImageField(upload_to='logo')
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, null=False, blank=False)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['category', ]),
+            models.Index(fields=['name', ])
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    seller = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=False, blank=False)
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.DO_NOTHING, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True)
+    deleted = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['name', 'deleted', ]),
+            models.Index(fields=['sub_category', 'deleted']),
+            models.Index(fields=['seller', 'deleted']),
+        ]
+
+
+class ProductPictures(models.Model):
+    picture = models.ImageField(upload_to='products')
+    product = models.ForeignKey(Products, on_delete=models.DO_NOTHING, null=False, blank=False)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['product', ]),
+        ]
+
+    def __str__(self):
+        return self.__str__() + self.product
