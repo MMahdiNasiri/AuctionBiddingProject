681af6482eba8e9ff8925bf1dda1ff012259c061
m.nasiri <mohammadmahdi.nasiri@digikala.ir>
2023-05-18 22:45:32 +0330

    feat: Add Models To Admin Panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Category, SubCategory, Products, ProductPictures
+
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Products)
+admin.site.register(ProductPictures)
